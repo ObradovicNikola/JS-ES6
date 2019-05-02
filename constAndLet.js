1172ec3a0e6d75dae358999b63a55a8489f9c74a
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-05-02 19:11:14 +0200

    Initial commit

---
+++ constAndLet.js
@@ -0,0 +1,15 @@
+//var x = 5;
+
+const PI = 3.14
+let k = 4
+
+function multiple(x) {
+    var y = 3;
+    return x*y;
+}
+
+function findCircleSurface(r) {
+    return  r*r*PI;
+}
+
+console.log(findCircleSurface(2));
